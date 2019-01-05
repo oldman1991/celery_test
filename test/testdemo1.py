3cc1d9d25f8f61e5087a4730b2303df6ab51e3a0
lipeng <lipeng@gmei.com>
2019-01-05 10:16:43 +0800

    去除.idea

--- test/testdemo1.py
+++ test/testdemo1.py
@@ -8,14 +8,23 @@ from celery_test.logger import info_logger
 
 @shared_task(bind=True)
 def add(self, x, y):
+    """
+    测试bind参数，打印task_id
+    调用方式 add.delay(1,2)
+    :param self:
+    :param x:
+    :param y:
+    :return:
+    """
     print(self.request.id)
     info_logger.debug(u"i am a bug")
     info_logger.info(u"i am a info")
     return x + y
 
-@shared_task
-def test_queue(x, y):
-    return x+y
 
 def excute_task():
-    test_queue.apply_async((1,3),queue='add')
+    """
+    测试使用apply_async方法。指定使用的队列
+    :return:
+    """
+    add.apply_async((1,3),queue='add')
